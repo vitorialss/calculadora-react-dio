dc5bf2c61487f26a41ffbc52f2367a85a49d6fca
vitorialss <107154247+vitorialss@users.noreply.github.com>
2022-10-26 23:05:34 -0300

    "finalizado mas ainda não testado"

---
+++ src/components/Input/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    
+    display: flex;
+    align-itens: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        padding: 0 10px;
+
+        display: flex;
+        flex-direction: column;
+        align-itens: flex-end;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF
+
+
+    }
+    `
---
+++ src/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #CACACA;
+
+    display: flex;
+    align-itens: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: #FFFFFF;
+    margin-top: 100px;
+    width: 50%;
+`
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-itens: center;
+`
+
+    export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-itens: center;
+`
